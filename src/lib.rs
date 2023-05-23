0eaf64ecb1a2cccb1fb7afde6c366e390b07fa28
Daniel Müller <deso@posteo.net>
2023-05-23 08:33:02 -0700

    Add initial project scaffolding
    This change introduces the basic Rust program scaffolding.

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+// Copyright (C) 2022 Daniel Mueller <deso@posteo.net>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#![allow(clippy::let_and_return, clippy::let_unit_value)]
+
+use anyhow::Result;
+
+
+/// Run the program and report errors, if any.
+pub fn run() -> Result<()> {
+  Ok(())
+}
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+// Copyright (C) 2022-2023 Daniel Mueller <deso@posteo.net>
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use anyhow::Result;
+
+use btrfs_backup::run;
+
+
+fn main() -> Result<()> {
+  run()
+}
