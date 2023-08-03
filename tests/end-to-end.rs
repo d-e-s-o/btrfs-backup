0a585a2daa9a882848e47a17da944927d4c5f9aa
Daniel Müller <deso@posteo.net>
2023-08-02 20:07:35 -0700

    Add tests for remote command functionality
    This change adds tests for the remote command functionality, recently
    introduced in the form of the --remote-command option added to various
    of the program's sub-commands. We do not really have a remote system and
    absolutely do not want to perform a live backup over SSH with
    credentials injected somehow. As the next best thing, we create a chroot
    with all necessary dependencies and make the chroot step the remote
    command. This way, for the purposes of our tests, we get full isolation
    from the actual system under test's file system.

--- tests/end-to-end.rs
+++ tests/end-to-end.rs
@@ -6,23 +6,292 @@
 //! End-to-end tests for `btrfs_backup`, mostly focused on argument
 //! handling.
 
+use std::collections::HashMap;
+use std::collections::HashSet;
+use std::env::split_paths;
+use std::env::var_os;
 use std::ffi::OsStr;
+use std::fs::copy as copy_file;
 use std::fs::create_dir_all;
+use std::fs::metadata;
 use std::fs::read_to_string;
 use std::fs::write;
+use std::fs::File;
+use std::io::BufRead as _;
+use std::io::BufReader;
+use std::io::ErrorKind;
 use std::os::unix::fs::symlink;
+use std::os::unix::fs::PermissionsExt as _;
 use std::path::Path;
+use std::path::PathBuf;
 
-use time::Duration;
+use anyhow::bail;
+use anyhow::Context as _;
+use anyhow::Error;
+use anyhow::Result;
 
+use glob::glob;
+use goblin::elf;
+use memmap::Mmap;
 use serial_test::serial;
+use time::Duration;
 
 use btrfs_backup::btrfs::Btrfs;
 use btrfs_backup::run;
 use btrfs_backup::snapshot::Snapshot;
 use btrfs_backup::test::with_btrfs;
 use btrfs_backup::test::with_two_btrfs;
+use btrfs_backup::test::Mount;
 use btrfs_backup::util::normalize;
+
+
+/// Collect all shared object search paths from an `ld.so.conf` style
+/// configuration file.
+fn collect_so_search_paths(ldso_conf: &Path, paths: &mut Vec<PathBuf>) -> Result<()> {
+  let ldso = File::open(ldso_conf)
+    .with_context(|| format!("failed to open {} for reading", ldso_conf.display()))?;
+  let reader = BufReader::new(ldso);
+  for line in reader.lines() {
+    let line = line.context("failed to reader line from /etc/ld.so.conf")?;
+    // Ignore comments.
+    if line.starts_with('#') {
+      continue
+    }
+
+    if let Some(include) = line.strip_prefix("include ") {
+      let dir = ldso_conf.parent().with_context(|| {
+        format!(
+          "ld.so.conf path {} does not have a parent",
+          ldso_conf.display()
+        )
+      })?;
+      let include = dir.join(include);
+      let include = include.to_str().with_context(|| {
+        format!(
+          "ld.so.conf configuration path {} is not a valid string",
+          include.display()
+        )
+      })?;
+      let () = glob(include)?.try_for_each(|result| {
+        let ldso_conf = result?;
+        let () = collect_so_search_paths(&ldso_conf, paths)?;
+        Result::<(), Error>::Ok(())
+      })?;
+    } else {
+      let () = paths.push(PathBuf::from(line));
+    }
+  }
+
+  Ok(())
+}
+
+
+/// Check whether a `path` represents a file or symlink.
+fn is_valid_file(path: &Path) -> Result<bool> {
+  match metadata(path) {
+    Ok(metadata) => Ok(metadata.is_file() || metadata.is_symlink()),
+    Err(err) if err.kind() == ErrorKind::NotFound => Ok(false),
+    Err(err) => Err(err.into()),
+  }
+}
+
+
+/// Resolve the path to a shared object.
+fn resolve_shared_object<S>(shared_object: S) -> Result<PathBuf>
+where
+  S: AsRef<OsStr>,
+{
+  let shared_object = shared_object.as_ref();
+
+  // See ld.so(8) for the proper procedure to follow for path
+  // resolution. We take tremendous short cuts here.
+  let mut search_dirs = Vec::new();
+  let ldso_conf = Path::new("/etc/ld.so.conf");
+  let () = collect_so_search_paths(ldso_conf, &mut search_dirs)?;
+
+  for search_dir in search_dirs {
+    let candidate = search_dir.join(shared_object);
+
+    if is_valid_file(&candidate)? {
+      return Ok(candidate)
+    }
+  }
+
+  bail!(
+    "shared object {} not found",
+    shared_object.to_string_lossy()
+  )
+}
+
+
+/// Check whether a `path` represents an executable file.
+fn is_executable_file(path: &Path) -> Result<bool> {
+  // This constant would be defined by `libc`, but we currently don't
+  // depend on that.
+  const S_IXUSR: u32 = 64;
+
+  match metadata(path) {
+    Ok(metadata) => {
+      if metadata.is_file() || metadata.is_symlink() {
+        let permissions = metadata.permissions();
+        Ok(permissions.mode() & S_IXUSR != 0)
+      } else {
+        Ok(false)
+      }
+    },
+    Err(err) if err.kind() == ErrorKind::NotFound => Ok(false),
+    Err(err) => Err(err.into()),
+  }
+}
+
+
+/// Resolve the absolute path to a binary.
+fn resolve_executable_path<E>(executable: E) -> Result<PathBuf>
+where
+  E: AsRef<OsStr>,
+{
+  let executable = executable.as_ref();
+
+  // TODO: It's unclear whether no `PATH` should be treated as error or
+  //       not.
+  for dir in split_paths(&var_os("PATH").unwrap_or_default()) {
+    let candidate = dir.join(executable);
+
+    if is_executable_file(&candidate)? {
+      return Ok(candidate)
+    }
+  }
+
+  bail!("executable {} not found", executable.to_string_lossy())
+}
+
+
+/// Collect the dependencies of an ELF binary into the provided
+/// `HashMap`.
+fn collect_deps(bin: PathBuf, deps: &mut HashMap<PathBuf, Vec<PathBuf>>) -> Result<()> {
+  if deps.contains_key(&bin) {
+    // We already covered this binary. No point in redoing that step.
+    return Ok(())
+  }
+
+  let file = File::open(&bin)?;
+  // SAFETY: The function is always safe to call.
+  let mmap = unsafe { Mmap::map(&file)? };
+
+  let elf = elf::Elf::parse(&mmap)?;
+  let mut libs = elf
+    .libraries
+    .iter()
+    .map(resolve_shared_object)
+    .collect::<Result<Vec<_>, _>>()?;
+
+  if let Some(interpreter) = elf.interpreter {
+    let () = libs.push(PathBuf::from(interpreter));
+  }
+
+  let _previous = deps.insert(bin, libs.clone());
+
+  let () = libs
+    .into_iter()
+    .try_for_each(|lib| collect_deps(lib, deps))?;
+
+  Ok(())
+}
+
+/// Copy a list of files below a destination directory.
+///
+/// This function assumes paths to those files are absolute and will
+/// establish the same directory hierarchy below `dest`.
+fn copy_files<F, P, D>(files: F, dest: D) -> Result<()>
+where
+  F: IntoIterator<Item = P>,
+  P: AsRef<Path>,
+  D: AsRef<Path>,
+{
+  let dest = dest.as_ref();
+  // Each file is expected to already be an absolute path.
+  for file in files {
+    let file = file.as_ref();
+    debug_assert!(file.is_absolute(), "{}", file.display());
+
+    // Create a relative path, so that we can join properly.
+    let rel = file.strip_prefix("/")?;
+    let dest_path = dest.join(rel);
+    // SANITY: We should always have a parent in `dest_path` at this
+    //         point.
+    let () = create_dir_all(dest_path.parent().unwrap())?;
+
+    let _count = copy_file(file, dest_path)?;
+  }
+  Ok(())
+}
+
+/// Copy a list of binary ELF files, including their ELF dependencies,
+/// below a destination directory.
+///
+/// This function assumes paths to those files are absolute and will
+/// establish the same directory hierarchy below `dest`.
+fn copy_with_deps<B, P, D>(bins: B, dest: D) -> Result<()>
+where
+  B: IntoIterator<Item = P>,
+  P: AsRef<Path>,
+  D: AsRef<Path>,
+{
+  let deps = bins.into_iter().try_fold(HashMap::new(), |mut deps, bin| {
+    let () = collect_deps(bin.as_ref().to_path_buf(), &mut deps)?;
+    Result::<HashMap<PathBuf, Vec<PathBuf>>>::Ok(deps)
+  })?;
+
+  let deps = deps
+    .into_iter()
+    .flat_map(|(binary, deps)| Some(binary).into_iter().chain(deps))
+    .collect::<HashSet<_>>();
+
+  copy_files(deps, dest)
+}
+
+
+/// Create and mount a btrfs file system and then create a chroot'able
+/// directory structure in there that includes all commands necessary
+/// for being on the receiving end of a `btrfs-backup` invocation (e.g.,
+/// the destination of a backup).
+fn with_btrfs_chroot<F>(f: F)
+where
+  F: FnOnce(&Path),
+{
+  with_btrfs(|root| {
+    // This is effectively a list of all command dependencies that
+    // btrfs-backup requires. `sh` and `env` are only needed due to
+    // test plumbing, though.
+    // TODO: It would be better to consume this list from the actual
+    //       program instead of maintaining it here.
+    let sh = resolve_executable_path("sh").unwrap();
+    let env = resolve_executable_path("env").unwrap();
+    let btrfs = resolve_executable_path("btrfs").unwrap();
+    let mkdir = resolve_executable_path("mkdir").unwrap();
+    let readlink = resolve_executable_path("readlink").unwrap();
+
+    let () = copy_with_deps([sh, env, btrfs, mkdir, readlink], root).unwrap();
+
+    let sh_run = Path::new(env!("CARGO_MANIFEST_DIR"))
+      .join("var")
+      .join("sh-run");
+
+    let bin_dir = root.join("bin");
+    let () = create_dir_all(&bin_dir).unwrap();
+    let _count = copy_file(sh_run, bin_dir.join("sh-run")).unwrap();
+
+    let proc_dir = root.join("proc");
+    let () = create_dir_all(&proc_dir).unwrap();
+    let _proc = Mount::builder()
+      .directory(proc_dir)
+      .arguments(["-t", "proc"])
+      .mount("proc")
+      .unwrap();
+
+    f(root)
+  })
+}
 
 
 /// Test that we can backup subvolumes with co-located snapshot
@@ -150,6 +419,32 @@ fn backup_subvolume_canonicalized() {
     ];
     let () = run(args).unwrap();
     assert_eq!(snapshots.read_dir().unwrap().count(), 1);
+  })
+}
+
+/// Check that we can backup a subvolume to a "remote" system.
+#[test]
+#[serial]
+fn backup_remote() {
+  with_btrfs(|src_root| {
+    with_btrfs_chroot(|dst_root| {
+      let btrfs = Btrfs::new();
+
+      let subvol = src_root.join("subvol");
+      let () = btrfs.create_subvol(&subvol).unwrap();
+
+      let remote_cmd = format!("chroot {} /bin/sh-run", dst_root.display());
+      let args = [
+        OsStr::new("btrfs-backup"),
+        OsStr::new("backup"),
+        subvol.as_os_str(),
+        OsStr::new("--destination"),
+        OsStr::new("/backups"),
+        OsStr::new("--remote-command"),
+        OsStr::new(&remote_cmd),
+      ];
+      let () = run(args).unwrap();
+    })
   })
 }
 
@@ -357,10 +652,64 @@ fn purge_destination_snapshots() {
   })
 }
 
+/// Check that we can purge old snapshots from a remote.
+#[test]
+#[serial]
+fn purge_remote() {
+  let tag = "";
+
+  with_btrfs_chroot(|root| {
+    let btrfs = Btrfs::new();
+    let subvol = root.join("some-subvol");
+
+    let snapshot1 = Snapshot::from_subvol_path(&subvol, tag).unwrap();
+    let mut snapshot2 = snapshot1.clone();
+    snapshot2.timestamp -= Duration::weeks(2);
+    let mut snapshot3 = snapshot1.clone();
+    snapshot3.timestamp -= Duration::weeks(4);
+    let mut snapshot4 = snapshot1.clone();
+    snapshot4.timestamp -= Duration::weeks(5);
+
+    let snapshots = [&snapshot1, &snapshot2, &snapshot3, &snapshot4];
+
+    let () = snapshots.iter().for_each(|snapshot| {
+      let subvol = root.join(snapshot.to_string());
+      let () = btrfs.create_subvol(&subvol).unwrap();
+      // Make the subvolumes read-only to fake actual snapshots.
+      let () = btrfs.make_subvol_readonly(&subvol).unwrap();
+    });
+
+    let non_existent = root.join("non-existent");
+
+    let remote_cmd = format!("chroot {} /bin/sh-run", root.display());
+    let args = [
+      OsStr::new("btrfs-backup"),
+      OsStr::new("purge"),
+      subvol.as_os_str(),
+      // We set a non-existing source repository here, to actually test
+      // that we purge from the destination and not the source
+      // repository.
+      OsStr::new("--source"),
+      non_existent.as_os_str(),
+      OsStr::new("--destination"),
+      OsStr::new("/"),
+      OsStr::new("--keep-for=3w"),
+      OsStr::new("--remote-command"),
+      OsStr::new(&remote_cmd),
+    ];
+    let () = run(args).unwrap();
+
+    assert!(root.join(snapshot1.to_string()).exists());
+    assert!(root.join(snapshot2.to_string()).exists());
+    assert!(!root.join(snapshot3.to_string()).exists());
+    assert!(!root.join(snapshot4.to_string()).exists());
+  })
+}
+
 /// Test that we can restore a subvolume.
 #[test]
 #[serial]
-fn restore_subvolumes() {
+fn restore_subvolume() {
   with_two_btrfs(|src_root, dst_root| {
     let btrfs = Btrfs::new();
 
@@ -550,6 +899,47 @@ fn restore_subvolume_canonicalized() {
 
     let content = read_to_string(subvol.join("file")).unwrap();
     assert_eq!(content, "test42");
+  })
+}
+
+/// Test that we can restore a subvolume from a remote.
+#[test]
+#[serial]
+fn restore_remote() {
+  with_btrfs(|src_root| {
+    with_btrfs_chroot(|dst_root| {
+      let btrfs = Btrfs::new();
+
+      let subvol = src_root.join("subvol");
+      let () = btrfs.create_subvol(&subvol).unwrap();
+
+      let remote_cmd = format!("chroot {} /bin/sh-run", dst_root.display());
+      let args = [
+        OsStr::new("btrfs-backup"),
+        OsStr::new("backup"),
+        subvol.as_os_str(),
+        OsStr::new("--destination"),
+        OsStr::new("/backups"),
+        OsStr::new("--remote-command"),
+        OsStr::new(&remote_cmd),
+      ];
+      let () = run(args).unwrap();
+
+      let () = btrfs.delete_subvol(&subvol).unwrap();
+
+      let args = [
+        OsStr::new("btrfs-backup"),
+        OsStr::new("restore"),
+        subvol.as_os_str(),
+        OsStr::new("--source"),
+        OsStr::new("/backups"),
+        OsStr::new("--remote-command"),
+        OsStr::new(&remote_cmd),
+      ];
+      let () = run(args).unwrap();
+
+      assert!(subvol.exists())
+    })
   })
 }
 
